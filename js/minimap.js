112b61ce47c7aef3d16d3efafe917784651cc665
pfruidur <83220786+pfruidur@users.noreply.github.com>
2023-03-15 21:19:21 +0100

    Añadido el minimapa con input de coordenadas

---
+++ js/minimap.js
@@ -0,0 +1,15 @@
+var map = L.map('map', {
+    center: [37.386188818756104, -5.992615333077015],
+    zoom: 5
+});
+
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap'
+        + '</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">'
+}).addTo(map)
+
+map.on('click', function(e) {
+    document.getElementById("entradaY").value = e.latlng.lat;
+    document.getElementById("entradaX").value = e.latlng.lng;
+});
